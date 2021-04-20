ecd5c3585fa6e056fc5165c40eb98436884e775a
GitHub Action <action@github.com>
2021-04-20 15:46:34 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sdadas/jasyptintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sdadas.jasyptintellijplugin.services
+
+import com.github.sdadas.jasyptintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/sdadas/jasyptintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sdadas/jasyptintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sdadas.jasyptintellijplugin.services
 
+import com.github.sdadas.jasyptintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
